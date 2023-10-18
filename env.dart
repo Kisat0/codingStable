3b200407204322ca4de5cbb5c8de90c50220d8e3
kisat0 <ghanembenyoucef@gmail.com>
2023-10-18 10:22:06 +0200

    adding database connection

---
+++ env.dart
@@ -0,0 +1,1 @@
+const MONGO_URL = "mongodb+srv://username:<password>@cluster0.gulei9j.mongodb.net/";
---
+++ mongodb.dart
@@ -0,0 +1,19 @@
+import 'env.dart';
+import 'dart:developer';
+import 'package:mongo_dart/mongo_dart.dart';
+
+
+class MongoDataBase {
+
+  static connect() async {
+    var db = await Db.create(MONGO_URL);
+    await db.open();
+    inspect(db);
+    var status = db.serverStatus();
+
+    inspect(status);
+
+    return db;
+  }
+  
+}
