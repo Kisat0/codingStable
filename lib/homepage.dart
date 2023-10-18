f7e7998e134def2a6d1c0d3d01a8aaeedc516d2e
lucas <lucasturschwell@hotmail.com>
2023-10-18 13:40:38 +0200

    homepage test

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -29,13 +29,43 @@ class _HomePageState extends State<HomePage> {
           NavigationDestination(
             selectedIcon: Icon(Icons.person),
             icon: Icon(Icons.person),
-            label: 'Connexion',
+            label: 'Inscription',
           ),
           NavigationDestination(
             selectedIcon: Icon(Icons.dashboard),
             icon: Icon(Icons.dashboard),
-            label: 'Évènements',
+            label: 'Connexion',
           ),
+          NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Flux d\'actualité'),
+          NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Profil'),
+          NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Mes cours'),
+          NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Concours'),
+          NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Soirée'),
+
+              NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Entraînement'),
+
+              NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Admin'),
         ],
       ),
       body: <Widget>[
@@ -50,6 +80,41 @@ class _HomePageState extends State<HomePage> {
         Container(
           alignment: Alignment.center,
           child: const Text('Page 3'),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 4'),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 5'),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 6'),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 7'),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 8'),
+        ),
+
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 9'),
+        ),
+
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 10'),
+        ),
+
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 11'),
         ),
       ][currentPageIndex],
     );
