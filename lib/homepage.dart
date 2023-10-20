90348fd70f8eddb49634218444997de93d0edc6b
lucas <lucasturschwell@hotmail.com>
2023-10-20 10:02:02 +0200

    prepare for merge on main

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -88,7 +88,7 @@ class _HomePageState extends State<HomePage> {
         ),
         Container(
           alignment: Alignment.center,
-          child: const Text('Page 2'),
+          child: const Text('Page 3'),
         ),
         Container(
           alignment: Alignment.center,
